816f9d224781f54c67b0ad1054f9c12e3c34aaa9
jvazquez94 <36034121+jvazquez94@users.noreply.github.com>
2021-01-19 17:40:21 +0100

    Add files via upload
    Funcionalidad que guarda las rutas de los ficheros de la carpeta actual donde se lanza.

---
+++ algoritmos_copia/GuardarPaths.java
@@ -0,0 +1,33 @@
+package urjc.isi;
+import java.io.*;
+
+public class GuardarPaths {
+	public static String arrayPaths[];//Declaramos el array donde guardaremos los paths
+	
+	public static void listarCarp() {
+		
+		String sCarp = System.getProperty("user.dir");//Obtenemos la carpeta actual desde donde ejecutamos el código(de la carpeta del proyecto)
+		File carpeta = new File(sCarp); //Con este File obtenemos información de la carpeta
+		String[] listado = carpeta.list();
+		arrayPaths = new String[listado.length];//Inicializo el array para que no salga error de NullPointerException
+		
+		if(listado==null || listado.length == 0) {
+			System.out.println("No hay elementos en la carpeta actual");
+			return;
+		}else {
+			for(int i=0; i<listado.length;i++) {
+				File f = new File(listado[i]);
+				arrayPaths[i]=f.getAbsolutePath();
+			}
+		}
+		for(int i=0; i<listado.length;i++) {
+			System.out.println(arrayPaths[i]);
+		}
+	}
+
+	
+	public static void main(String[] args) {
+		listarCarp();
+	}
+
+}
