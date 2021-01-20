4104013c1f5ac850ea59dba47f98aad80933fb31
Samuel <47353257+samuelcobos@users.noreply.github.com>
2021-01-20 13:51:18 +0100

    Update alumnoDao.java

--- src/main/java/urjc/isi/servidor/alumnoDao.java
+++ src/main/java/urjc/isi/servidor/alumnoDao.java
@@ -26,7 +26,7 @@ public class alumnoDao {
         	c = DriverManager.getConnection(dbUrl,username,password);
             c.setAutoCommit(false);
 
-            c.prepareStatement("drop table if exists Alumnos").execute();
+            c.prepareStatement("drop table if exists Alumnos CASCADE").execute();
             c.prepareStatement("CREATE TABLE Alumnos (idAlumno	VARCHAR(50) NOT NULL UNIQUE,Nombre	VARCHAR(50) NOT NULL,Puerto	INTEGER,IP	VARCHAR(50),PRIMARY KEY(idAlumno))").execute();
 	
             c.commit();
