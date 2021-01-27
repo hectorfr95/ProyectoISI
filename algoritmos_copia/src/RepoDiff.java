52d331703f864cbd5852d682f6092b5b873d7366
RazvanCopaceanu <35956444+RazvanCopaceanu@users.noreply.github.com>
2021-01-27 09:47:08 +0100

    Añadidas notas de explicación

--- algoritmos_copia/src/RepoDiff.java
+++ algoritmos_copia/src/RepoDiff.java
@@ -1,3 +1,7 @@
+/*
+ * Clase que contiene un método que compara repositorios de ficheros mediante funciones de JGit 
+ */
+
 package urjc.isi.pruebasJGit;
 
 import java.io.ByteArrayOutputStream;
@@ -20,29 +24,29 @@ public class RepoDiff {
 	public void compare(byte[] fileContentOld, byte[] fileContentNew) {
 
 		try {
-
+			//Crea un nuevo repositorio temporal
 			Repository repository = createNewRepository();
 			Git git = new Git(repository);
 			
-			
+			//Crea un nuevo fichero en el repositorio temporal y hace commit
 			commitFileContent(fileContentOld, repository, git, true);
 			commitFileContent(fileContentNew, repository, git, true);
 	    	
 						
-	        
+	        //Devuelve el identificador inferior del árbol en vez del identificador del propio commit
 	        ObjectId oldHead = repository.resolve("HEAD~1^{tree}");
 	        ObjectId head = repository.resolve("HEAD^{tree}");
 
 	        System.out.println("Printing diff between tree: " + oldHead + " and " + head);
 
-	       
+	       		//Prepara unos iteradores para poder calcular los diffs entre repositorios
 			ObjectReader reader = repository.newObjectReader();
 			CanonicalTreeParser oldTreeIter = new CanonicalTreeParser();
 			oldTreeIter.reset(reader, oldHead);
 			CanonicalTreeParser newTreeIter = new CanonicalTreeParser();
 			newTreeIter.reset(reader, head);
 
-			
+			//Recoge la lista de ficheros cambiados de un repositorio
 			List<DiffEntry> diffs= new Git(repository).diff()
 			                    .setNewTree(newTreeIter)
 			                    .setOldTree(oldTreeIter)
@@ -59,11 +63,11 @@ public class RepoDiff {
 
 	
 	public static Repository createNewRepository() throws IOException {
-		
+		//Prepara la ruta para el nuevo directorio
 		File localPath = File.createTempFile("TestGitRepository", "");
 		localPath.delete();
 
-	
+		//Crea el nuevo directorio temporal
 		Repository repository = FileRepositoryBuilder.create(new File(
 				localPath, ".git"));
 		repository.create();
@@ -72,7 +76,7 @@ public class RepoDiff {
 	}
 	
 	
-	
+	//Añade un fichero y hace commit, a un repositorio del sistema de control de versiones
 	private void commitFileContent(byte[] fileContent, Repository repository, Git git, boolean commit) throws Exception {
 		
 		File myfile = new File(repository.getDirectory().getParent(), "testfile");
@@ -83,11 +87,11 @@ public class RepoDiff {
 		baos.write(fileContent);
 		baos.writeTo(fos);
 		
-		
+		//Se hace git add, para añadir el fichero al sistema de control de versiones
 		git.add().addFilepattern("testfile").call();
 
 		if(commit) {
-			
+			//hace commit a los cambios
 			git.commit().setMessage("Added fileContent").call();
 		}
 		
