0169f7a67f809072f4c50f2f184de565b97e5b6e
RocioNaVi <r.navarro.2016@alumnos.urjc.es>
2021-01-29 12:06:15 +0100

    Añado comentarios y modificacion del inicio y fin de la conexion

--- src/main/java/urjc/isi/servidor/alumnoDao.java
+++ src/main/java/urjc/isi/servidor/alumnoDao.java
@@ -1,6 +1,6 @@
-package urjc.isi.servidor;
+age urjc.isi.servidor;
 
-import java.net.URI;
+
 import java.net.URISyntaxException;
 import java.sql.*;
 import java.util.ArrayList;
@@ -10,14 +10,20 @@ import java.util.List;
 public class alumnoDao {
 	
 	private static Connection c;
+	
+    // Este método abre la conexion con la bbdd
+    public void abrirConexion(Connection conn) throws SQLException {
+    	
+        conn = DriverManager.getConnection("jdbc:sqlite:proyecto.db");
+        conn.setAutoCommit(false);
+    }
 
-	// Con este método creamos la conexión con la bbdd.
+	// Con este método creamos la conexión con la bbdd
+	// Se crea la tabla Alumnos (si ya estaba creada se elimina)
     public alumnoDao() throws URISyntaxException {
         try {
             if(c!=null) return;
-
-            c = DriverManager.getConnection("jdbc:sqlite:proyecto.db");
-            c.setAutoCommit(false);
+            abrirConexion(c);
 
             c.prepareStatement("drop table if exists Alumnos").execute();
             c.prepareStatement("CREATE TABLE Alumnos (idAlumno	VARCHAR(50) NOT NULL UNIQUE,Nombre	VARCHAR(50) NOT NULL,Puerto	INTEGER,IP	VARCHAR(50),PRIMARY KEY(idAlumno))").execute();
@@ -29,16 +35,24 @@ public class alumnoDao {
         }
     }
     
-   
+    // Este método cierra las conexiones con la bbdd, para que no surjan bloqueos.
+    public void cerrarConexion(Connection conn, PreparedStatement ps, ResultSet rs) throws SQLException {
+    	
+    	if(rs != null) {
+    		rs.close();
+    	}
+        ps.close();
+        conn.close();
+    }
 
-    // Con este método vamos a poder obtener todos alumnos de la tabla Alumnos.
+    // Con este método vamos a poder obtener todos los alumnos de la tabla Alumnos.
     public List<alumno> all() {
 
         List<alumno> allAlumnos = new ArrayList<alumno>();
 
         try {
-        	c = DriverManager.getConnection("jdbc:sqlite:proyecto.db");
-            c.setAutoCommit(false);
+        	abrirConexion(c);
+        	// Esta es la query de sql
             PreparedStatement ps = c.prepareStatement("select * from Alumnos");
 
             ResultSet rs = ps.executeQuery();
@@ -49,9 +63,7 @@ public class alumnoDao {
                 String ip = rs.getString("ip");
                 allAlumnos.add(new alumno(idAlumno, nombre, puerto, ip));
             }
-            rs.close();
-            ps.close();
-            c.close();
+            cerrarConexion(c, ps, rs);
         } catch (SQLException e) {
             throw new RuntimeException(e);
         } finally {
@@ -67,9 +79,8 @@ public class alumnoDao {
     	int puerto = 0;
     	
         try {
-        	c = DriverManager.getConnection("jdbc:sqlite:proyecto.db");
-            c.setAutoCommit(false);
-            
+        	abrirConexion(c);
+        	// Esta es la query de sql
         	String query = "SELECT * from Alumnos WHERE idAlumno = " + idAlumno;
             PreparedStatement ps = c.prepareStatement(query);
             	
@@ -79,9 +90,7 @@ public class alumnoDao {
                 ip = rs.getString("Ip");
                 puerto = rs.getInt("puerto");
             }
-            rs.close();
-            ps.close();
-            c.close();
+            cerrarConexion(c, ps, rs);
         } catch (SQLException e) {
             throw new RuntimeException(e);
         } finally {
@@ -93,8 +102,8 @@ public class alumnoDao {
     // Con este método insertamos el objeto alumno recibido en la tabla de nuestra bbdd Alumnos.
     public void save(alumno alumno) {
         try {
-        	c = DriverManager.getConnection("jdbc:sqlite:proyecto.db");
-            c.setAutoCommit(false);
+        	abrirConexion(c);
+        	// Esta es la query de sql
             PreparedStatement ps = c.prepareStatement("insert into Alumnos(idAlumno, nombre , puerto, ip) VALUES(?,?,?,?)");
             ps.setString(1, alumno.getIdAlumno());
             ps.setString(2, alumno.getNombre());
@@ -102,22 +111,13 @@ public class alumnoDao {
             ps.setString(4, alumno.getIp());
             
             ps.execute();
+            // trazas
             System.out.println("sql.DNI alumno: "+  alumno.getIdAlumno());
             System.out.println("sql.NOMBRE alumno: "+  alumno.getNombre());
             System.out.println("sql.IP: "+  alumno.getIp());
             System.out.println("sql.PUERTO: "+  alumno.getPuerto());
             c.commit();
-            ps.close();
-            c.close();
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    // Cerramos la conexión con la bbdd
-    public void close() {
-        try {
-            c.close();
+            cerrarConexion(c, ps, null);
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
