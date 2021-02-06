7b4776ea42586b848a96f84c01bb60229a3dcfd8
jmruizc2017 <60428521+jmruizc2017@users.noreply.github.com>
2021-02-06 13:08:31 +0100

    Principal actualizado

--- algoritmos_copia/Principal.java
+++ algoritmos_copia/Principal.java
@@ -1,6 +1,9 @@
-import java.io.BufferedReader;
+package Proyecto2.Principla2;
+
+
 import java.io.IOException;
-import java.io.InputStreamReader;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
@@ -8,19 +11,26 @@ import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
+import java.util.Random;
+
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.errors.GitAPIException;
+import org.eclipse.jgit.api.errors.NoHeadException;
+import org.eclipse.jgit.lib.Repository;
+
+import org.eclipse.jgit.revwalk.RevCommit;
 
 
 
 
-public class Principal {
+public class Principal /*extends Repository */{
 	
-	private static final String ARCHIVO_ZIP = "https://wordpress.org/latest.zip";
-	private static final String RUTA_SALIDA = "/home/juanmanuel/Escritorio/prueba";
-	private static final String DIRECTORIO = "/home/juanmanuel/Escritorio/prueba/gits";
+	//private static final String ARCHIVO_ZIP = "https://wordpress.org/latest.zip";
+	private static final String PATH_LOCAL = "/home/alumno/Escritorio/prueba";
+	private static final String DIRECTORIO = "/home/alumno/Escritorio/prueba/gits";
 	
 	public static void CrearDirectorio (String args){
         File directorio = new File(args);
@@ -33,7 +43,7 @@ public class Principal {
             }
         }
     }
-    
+    /*
     public static void EjecutarWget(){
 		try {
 			String[] cmd = {"wget","-P",RUTA_SALIDA,ARCHIVO_ZIP};
@@ -47,7 +57,7 @@ public class Principal {
             e.printStackTrace();
         }
 	}
-	
+	*/
 	public static String[] ObtenerFich(String dir_path) {
 
 		String[] arr_res = null;
@@ -107,33 +117,83 @@ public class Principal {
 
 		}
 	}
+    
+    public static void Diffs(String commit1, String commit2, String file) throws IOException, GitAPIException {
+    	 try (Repository repo = CookbookHelper.openJGitCookbookRepository()) {
+			DiffRenamed diffrenamed = new DiffRenamed();
+			diffrenamed.runDiff(repo, commit1, commit2, file);
+    	 }
+    }
+    
+    //Metodo que recibe el path (ruta al directorio .git) de tipo String y lista todos los commits del directorio 	
+	public static void listCommits(String path) throws IOException, NoHeadException, GitAPIException {
 	
+		String commitId;
+		String author = null;
+		ArrayList<String> lista = new ArrayList<String>();
+	    Path repoPath = Paths.get(path);
+	
+	    try (Git git = Git.open(repoPath.toFile())) {
+		  // all
+		  Iterable<RevCommit> logs = git.log().all().call();
+		  for (RevCommit rev : logs) {
+		    commitId = rev.getId().getName();
+		    lista.add(commitId);
+		    author = rev.getAuthorIdent().getName();
+		  }/*
+		  Iterator i = lista.iterator();
+		  while(i.hasNext()) {
+			  System.out.println(i.next());
+			  
+		  }*/
+		  Object[] list = lista.toArray();
+		  for(int j = 1; j < list.length; j++) {
+			String str1 = (String)list[j-1];
+			String str2 = (String)list[j];
+			Diffs(str1, str2, "f2");
+		  }
+	    }
+	}
+	
+	public static String NombreAleatorio() {
+		char n;
+		
+		Random rnd = new Random();
+		String cadena = new String();
+		n = (char)(rnd.nextDouble()*26.0 + 65.0);
+		cadena += n;
+		System.out.println("Random: " + cadena);
+		return cadena;
+	}
 
-	
     public static void main(String[] args) throws Exception{
-		CrearDirectorio(DIRECTORIO);
-        EjecutarWget();
-        String path = RUTA_SALIDA;
-        String[] files = ObtenerFich(path);
+    	CrearDirectorio(DIRECTORIO);
+        //EjecutarWget();
+        String[] files = ObtenerFich(PATH_LOCAL);
         
 		if (files != null) {
 			int size = files.length;
             for (int i = 0; i < size; i++) {
-				System.out.println(files[i]);
-                List listaArchivos;
+				System.out.println("Imprimiendo: "+ files[i]);
                 String ARCHIVO_ZIP = files[i];
+                //String cadena = NombreAleatorio();
+                //String CARPETA = DIRECTORIO.concat("/" + cadena);
+                //CrearDirectorio(DIRECTORIO);
                 Descomprimir(ARCHIVO_ZIP,DIRECTORIO);
 				
 				File carpeta = new File(DIRECTORIO);
 				String[] listado = carpeta.list();
 				
 				for (i = 0; i < listado.length; i++){
-					System.out.println(listado[i]);
+					System.out.println("Lista: "+ listado[i]);
 					String DIRECTORIO2 = DIRECTORIO.concat("/" + listado[i]);
+					System.out.println(DIRECTORIO2);
+					listCommits(DIRECTORIO2);
+					System.out.println("Fin " + i + DIRECTORIO2);
+					
 				}
 
 			}
 		}
     }
 }
-
